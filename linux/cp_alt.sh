31079cb2e5e33358f4f28222e59a0b1a80dc3abf
theoka <theoka@users.noreply.github.com>
2014-12-31 14:24:50 -0500

    Create cp_alt.sh

---
+++ linux/cp_alt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# For copying many larger files (movies, music)
+# so that memory doesn't get devoured like usual
+# with the cp command
+
+if [ $# -lt 1 ]
+then
+    echo "Not enough args (need -help?)"
+elif [ $1 == "-help" ] || [ $1 == "-h" ]
+then
+    echo "usage: cp_asynchrous.sh /path/to/dir_a /path/to/dir_b"
+else
+    for fname in `ls $1`
+    do cp -r $1/$fname $2; done
+fi
