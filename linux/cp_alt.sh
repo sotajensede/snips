1fd8d931093841581b85398bdf0ba3dc08f59fca
theoka <theoka@users.noreply.github.com>
2014-12-31 14:25:33 -0500

    Update cp_alt.sh

--- linux/cp_alt.sh
+++ linux/cp_alt.sh
@@ -8,7 +8,7 @@ then
     echo "Not enough args (need -help?)"
 elif [ $1 == "-help" ] || [ $1 == "-h" ]
 then
-    echo "usage: cp_asynchrous.sh /path/to/dir_a /path/to/dir_b"
+    echo "usage: cp_alt.sh /path/to/dir_a /path/to/dir_b"
 else
     for fname in `ls $1`
     do cp -r $1/$fname $2; done
